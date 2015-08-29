3e7b8ab61ba603fa3b3888a4b1c3766d8d8c3410
Marek Mikołajczyk <marek.mikolajczyk@netguru.pl>
2015-08-29 13:41:39 +0200

    Factories for user, book and book category

---
+++ spec/factories/book.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define do
+  factory :book do
+    author { FFaker::Name.name }
+    publishing_house { FFaker::Company.name }
+    published_at { 10.years.ago }
+    quantity 1
+
+    ignore do
+      book_category_name nil
+    end
+
+    before(:create) do |book, evaluator|
+      if evaluator.book_category_name
+        create :book_category, name: evaluator.book_category_name
+        book.book_category = category
+      end
+    end
+  end
+end
